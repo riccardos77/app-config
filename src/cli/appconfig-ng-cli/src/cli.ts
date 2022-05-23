77f4a6752e339cf0a06970e23e8da6cfc09492b2
Riccardo Sale <riccardos@live.com>
2022-05-23 17:59:56 +0200

    ng-cli: added tag models generation / fix npx run

--- src/cli/appconfig-ng-cli/src/cli.ts
+++ src/cli/appconfig-ng-cli/src/cli.ts
@@ -1,40 +1,69 @@
+#! /usr/bin/env node
 import { readFileSync, writeFileSync } from 'fs';
 import { JSONSchema4 } from 'json-schema';
 import { compile, Options } from 'json-schema-to-typescript';
 import { Options as PrettierOptions } from 'prettier';
 import request from 'request';
 
-const config: CliSettings = JSON.parse(readFileSync(process.cwd() + '\\.appconfig-cli.json').toString()) as CliSettings;
+const configFilePath = process.cwd() + '/' + (process.argv[2] ?? '.appconfig-cli.json');
+console.log('reading configuration from ' + configFilePath);
 
-const url = `${config.origin.configInstanceUrl}apps/${config.origin.appName}/values/instances/${config.origin.appIdentity}/schema`;
-request({ uri: url }, function (error: unknown, response: unknown, body: string) {
-  if (error !== undefined && error !== null) {
-    process.stderr.write('Error downloading schema from ' + url);
-    if (typeof error === 'string') {
-      process.stderr.write(error);
-    }
-    return;
-  }
+const config: CliSettings = JSON.parse(readFileSync(configFilePath).toString()) as CliSettings;
 
-  const json2tsOptions = Object.assign<unknown, Partial<Options>, Partial<Options>>(
-    {},
-    { bannerComment: '', strictIndexSignatures: true },
-    config.json2ts
-  );
-  if (config.json2ts.styleFile) {
-    json2tsOptions.style = JSON.parse(readFileSync(config.json2ts.styleFile).toString()) as PrettierOptions;
-  }
+const json2tsOptions = Object.assign<{}, Partial<Options>, Partial<Options>>(
+  {},
+  { bannerComment: '', strictIndexSignatures: true },
+  config.json2ts
+);
 
-  compile(JSON.parse(body) as JSONSchema4, '', json2tsOptions)
-    .then(code => {
-      writeFileSync(config.destination, code, { encoding: 'utf-8' });
-    })
-    .catch((err: unknown) => {
-      process.stderr.write('Error generating types from schema');
-      if (typeof err === 'string') {
-        process.stderr.write(err);
+if (config.json2ts.styleFile) {
+  json2tsOptions.style = JSON.parse(readFileSync(config.json2ts.styleFile).toString()) as PrettierOptions;
+}
+
+async function compileSchemaAsync(url: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    request({ uri: url }, function (error: unknown, response: unknown, body: string) {
+      if (error !== undefined && error !== null) {
+        process.stderr.write('Error downloading schema from ' + url);
+        if (typeof error === 'string') {
+          process.stderr.write(error);
+        }
+
+        reject(error);
+        return;
       }
+
+      compile(JSON.parse(body) as JSONSchema4, '', json2tsOptions)
+        .then(code => {
+          resolve(code);
+          return;
+        })
+        .catch((err: unknown) => {
+          process.stderr.write('Error generating types from schema');
+          if (typeof err === 'string') {
+            process.stderr.write(err);
+          }
+
+          reject(err);
+          return;
+        });
     });
+  });
+}
+
+let allCode = '';
+
+void compileSchemaAsync(
+  `${config.origin.configInstanceUrl}apps/${config.origin.appName}/values/instances/${config.origin.appIdentity}/schema`
+).then(r => {
+  allCode += r;
+
+  void compileSchemaAsync(
+    `${config.origin.configInstanceUrl}apps/${config.origin.appName}/values/instances/${config.origin.appIdentity}/schema/tags`
+  ).then(r2 => {
+    allCode += '\n' + r2;
+    writeFileSync(config.destination, allCode, { encoding: 'utf-8' });
+  });
 });
 
 interface CliSettings {
