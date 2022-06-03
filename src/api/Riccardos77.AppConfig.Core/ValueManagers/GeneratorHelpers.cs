31dcd7a3aeacd721049aede2fef6780ebd83b819
Riccardo Sale <riccardos@live.com>
2022-06-03 17:07:05 +0200

    tags schema: removed additional properties allowance

--- src/api/Riccardos77.AppConfig.Core/ValueManagers/GeneratorHelpers.cs
+++ src/api/Riccardos77.AppConfig.Core/ValueManagers/GeneratorHelpers.cs
@@ -10,6 +10,7 @@ internal class GeneratorHelpers
         var schemaTags = new JSchema
         {
             Type = type,
+            AllowAdditionalProperties = false
         };
 
         if (conf.Tags is not null)
