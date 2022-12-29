3ce4096d88acd64487831040ac1b109d3b4c8d1f
Riccardo Sale <riccardos@live.com>
2022-12-30 00:01:28 +0100

    release 0.4.0

--- deploy/docker-publish.bat
+++ deploy/docker-publish.bat
@@ -1,4 +1,4 @@
 @REM docker tag riccardos77-appconfig riccardos77/app-config:latest
-docker push riccardos77/app-config:0.3.0
-docker tag riccardos77/app-config:0.3.0 riccardos77/app-config:latest
+docker push riccardos77/app-config:%1
+docker tag riccardos77/app-config:%1 riccardos77/app-config:latest
 docker push riccardos77/app-config:latest
